c80b4eb8f2c219f67da087075a8b0861d0769826
Kamiii-A <Kamiii-A>
2019-10-08 19:14:37 +0800

    新建的js作业库

---
+++ work2/js/index.js
@@ -0,0 +1,17 @@
+
+//检测用户输入的内容注册并登陆
+function checkpassword() {
+    var password = prompt('请输入密码以注册并登陆！输入空密码将无法注册！');
+    if (password != '' && password != null) {
+        alert('注册成功!');
+        var register = document.getElementById("button_register");
+        var talkingroom = document.getElementById("button_talkingroom");
+        var storage = true;
+        register.setAttribute("disabled", "");
+        talkingroom.removeAttribute("disabled");
+    }
+}
+//控制按钮变色
+function color(str) {
+    document.body.style.backgroundColor = str;
+}
---
+++ work2/js/talking room.js
@@ -0,0 +1,4 @@
+function yhinput(name){
+    var talk = prompt('你想说啥？')
+    console.log(name + talk);
+}
